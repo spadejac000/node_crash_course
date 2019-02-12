f67385ff5399e26abbded502d884eb267487c00d
Jacob Spade <spade.jacob@Yahoo.com>
2019-02-11 23:51:02 -0800

    handling file types in server file

--- server.js
+++ server.js
@@ -3,9 +3,49 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('<h1>Home</h1>');
+  // if(req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, {'Content-Type': 'text/html'})
+  //     res.end(content);
+  //   })
+  // }
+
+  // if(req.url === '/api/users') {
+  //   const users = [
+  //     { name: 'Bob Smith', age: 40},
+  //     { name: 'John Doe', age: 30}
+  //   ];
+  //   res.writeHead(200, {'Content-Type': 'application/json'});
+  //   res.end(JSON.stringify(users));
+  // }
+
+  // build file path
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  // extension of the file
+  let extname = path.extname(filePath);
+
+  // initial content type
+  let contentType = 'text/html';
+
+  // check extension and set content type
+  switch(extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
   }
 });
 
